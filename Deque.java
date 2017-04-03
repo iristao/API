e84a4e37d499f4cb647f943f6cd4245cb26ecc55
pac-yan <pyan@stuy.edu>
2017-04-03 09:46:24 -0400

    Skelly

--- Deque.java
+++ Deque.java
@@ -6,12 +6,49 @@
 */
 
 public interface Deque<D>{
+    /*-------------Begin Add Methods[Iris]------------*/
+    /*addFirst:
+    ---inserts the specified element at the front of this deque if it is possible todo so immediately without violating capacity restrictions.
+    */
+    public void addFirst(T x){
 
-    //addFirst
-    //pollFirst
-    //peekFirst
+    }
+    /*addLast:
+    ---inserts the specified element at the end of this deque if it is possible to do so without violating capacity restrictions
+    */
+    public void addlast(T x){
 
-    //addLast
-    //pollLast
-    //peekLast
+    }
+    /*-------------End Add Methods------------*/
+
+
+
+    /*-------------Begin Poll Methods[Pacy]------------*/
+    /*pollFirst:
+    ---retrieves and removes the first element of this deque, or returns null if this deque is empty.*/
+    public T pollFirst(){
+
+    }
+    /*pollLast:
+    ---retrieves and removes the last element of this deque, or returns null if this deque is empty*/
+    public T pollLast(){
+
+    }
+    /*-------------End Poll Methods------------*/
+
+
+
+    /*-------------Begin Peek Methods[]------------*/
+
+    /*peekFirst:
+      ---Retrieves, but does not remove, the first element of this deque, or returns null if this deque is empty*/
+    public T peekFirst(){
+	
+    }
+    /*peekLast:
+      ---Retrieves, but does not remove, the last element of this deque, or returns null if this deque is empty*/
+    public T peekLast(){
+	
+    }
+    /*-------------End Peek Methods------------*/
 }

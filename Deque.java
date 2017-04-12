f52181ee0d2549bba39adf6ee550d04fc640bec0
Iris Tao <mtao@stuy.edu>
2017-04-12 01:52:51 -0400

    typo fixed

--- Deque.java
+++ Deque.java
@@ -5,25 +5,25 @@
     Comment out all methods.
 */
 
-public interface Deque<D>{
+public interface Deque<E>{
     /*inserts an element at the front the deque*/
-    public void addFirst(T x);
+    public void addFirst(E x);
 
     /*inserts an element at the end of the deque*/
-    public void addlast(T x);
+    public void addLast(E x);
 
 
 
     /*returns and removes the first element of the deque - returns null if deque is empty*/
-    public T pollFirst();
+    public E pollFirst();
 
     /*returns and removes the last element of the deque - returns null if deque is empty*/
-    public T pollLast();
+    public E pollLast();
 
 
     /*returns the first element of the deque - returns null if deque is empty*/
-    public T peekFirst();
+    public E peekFirst();
 
     /*returns the last element of the deque - returns null if deque is empty*/
-    public t peekLast();
+    public E peekLast();
 }
